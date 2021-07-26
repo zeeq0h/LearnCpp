ecc4f0877496e29dcfd80498c1a2c1697167007d
zeeq0h <logannoonan@outlook.com>
2021-07-26 22:07:56 +0100

    moved files to appropriate folder

--- lesson 9 multidimensional arrays.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    const int rows {3};
-    const int cols {4};
-    int movie_rating [rows] [cols];
-    //this creates a table with 3 rows and 4 collumns 
-}
