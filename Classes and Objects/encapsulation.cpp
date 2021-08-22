df49097144d100cdaef9afd7f791ce6e9a5fc41d
zeeq0h <logannoonan@outlook.com>
2021-08-22 17:24:21 +0100

    updated

--- Classes and Objects/encapsulation.cpp
+++ Classes and Objects/encapsulation.cpp
@@ -1,3 +1,8 @@
+/*
+A private member cannot be accessed, or even viewed, from outside the class; it can be accessed only from within the class.
+A public member function may be used to access the private members. For example:
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
